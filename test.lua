26bb224c3f0638fa39815aa6f0fbab170ac6925f
Tai @ Flex <dch.tai@gmail.com>
2016-09-15 17:42:04 +0100

    examples

--- test.lua
+++ test.lua
@@ -2,14 +2,30 @@ mobs.override(
 	"dmobs:panda",
 	{
 		hp_max = 120,
-		---[[
+		--[[
 		on_rightclick = {
 			fchain_type = "before",
 			fchain_func = function(self,clicker)
-				--minetest.chat_send_player(clicker.get_player_name(),"Right clicked a panda!")
+				minetest.chat_send_player(clicker:get_player_name(),"Right clicked a panda!")
 				return false
 			end
 		}
 		--]]
 	}
 )
+
+-- get necessary elements
+local themob = minetest.registered_entities["dmobs:panda"]
+
+local thefunc = function(self,clicker)
+	minetest.chat_send_player(clicker:get_player_name(),"Right clicked a panda!")
+	return true
+end
+
+-- assign arbitrary function
+
+local orig = themob.on_rightclick
+
+themob.on_rightclick = function(...)
+	if thefunc(unpack(...)) then orig(unpack(...)) end
+end
