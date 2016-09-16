221d4bc2d36e175461df2d4e8bec7afcfeccef85
Tai @ Flex <dch.tai@gmail.com>
2016-09-16 11:21:45 +0100

    demo

--- example.lua
+++ example.lua
@@ -1,13 +1,17 @@
-mobs.override(
-	"dmobs:panda",
-	{
-		hp_max = 120,
-		on_rightclick = {
-			fchain_type = "before",
-			fchain_func = function(self,clicker)
-				minetest.chat_send_player(clicker:get_player_name(),"Panda power!")
-				return true
-			end
-		}
+
+local demofunction = function(self,clicker)
+	minetest.chat_send_player(clicker:get_player_name(),"You right-clicked a "..self.name)
+	return true
+end
+
+local genrewrite = {
+	hp_max = 120,
+	type="npc",
+	on_rightclick = {
+		fchain_type = "before",
+		fchain_func = demofunction
 	}
-)
+}
+
+mobs.override("dmobs:panda",genrewrite)
+mobs.override("dmobs:fox",genrewrite)
